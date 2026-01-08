e4a5594a358abc7f65721d82d18676896487e3fd
Stanislav Haluska <stanislav.haluska@gmail.com>
2026-01-08 11:13:28 +0100

    support ir12 04.10

--- custom_components/regulus/const.py
+++ custom_components/regulus/const.py
@@ -1,12 +1,13 @@
 DOMAIN = "regulus"
 NAME = "Regulus Heat Pump"
 COMPANY = "Regulus"
-IR_VERSION_OPTIONS = ["12", "14_1_0_10_0"]
+IR_VERSION_OPTIONS = ["12_04_10", "14_1_0_10_0"]
 POLLING_INTERVAL = 5
 IR_VERSION_ALIASES = {
-    12: "12",
+    12: "12_04_10",  # default mapping for legacy users
     14: "14_1_0_10_0",  # default mapping for legacy users
-    "12": "12",
-    "14": "14_1_0_10_0",  # default mapping for legacy users
+    "12": "12_04_10",
+    "14": "14_1_0_10_0",
+    "12_04_10": "12_04_10",
     "14_1_0_10_0": "14_1_0_10_0",
 }
---
+++ custom_components/regulus/mapper/registry_mapper_ir12_04_10.py
@@ -0,0 +1,72 @@
+REGISTRY_MAPPER = {
+  # dashboard
+  "outdoorTemperature": "__R22657_REAL_.1f",
+  "rcTariff": "__R10949.2_BOOL_i",
+  "holiday": "__R8266.1_BOOL_i",
+  "heatPumpRunningStatus": "__R8492.0_BOOL_i",
+  "heatPumpOutletTemperature": "__R8460_INT_-4.1d",
+  "heatPumpInletTemperature": "__R8456_INT_-4.1d",
+  "waterRunningStatusFromHeatPump": "__Y328.2_BOOL_i",
+  "circulationRunningStatus": "__R10949.0_BOOL_i",
+  "zone1RunningStatus": "__R2396_USINT_u",
+  "zone1RequiredTemperature": "__R22831_REAL_.1f",
+  "zone1HeatingWaterTemperature": "__R22709_REAL_.1f",
+  "zone1RequiredHeatingWaterTemperature": "__R22807_REAL_.1f",
+  "zone2RunningStatus": "__R18045_USINT_u",
+  "zone2Temperature": "__R128170_REAL_.1f",
+  "zone2RequiredTemperature": "__R8457_REAL_.1f",
+  "zone2HeatingWaterTemperature": "__R8331_REAL_.1f",
+  "zone2RequiredHeatingWaterTemperature": "__R8433_REAL_.1f",
+  "akuTopTemperature": "__R8275_REAL_.1f",
+  "akuBottomTemperature": "__R22665_REAL_.1f",
+  "akuRequiredTemperature": "__R22795_REAL_.1f",
+  "akuRunningStatusFromHeatPump": "__R79399.0_BOOL_i",
+  "solarPanelTemperature": "__R8319_REAL_.1f",
+  "solarRunningStatus": "__R12018.4_BOOL_i",
+
+  #heat pump
+  "hpRunningTime": "__R8966_TIME_Thh:mm:ss",
+  "hpIdleTime": "__R8978_TIME_Thh:mm:ss",
+  "overallTotalHours": "__R8940_UDINT_u",
+  "overallTotalStarts": "__R8950_UINT_u",
+  "overallTodayHours": "__R8912_USINT_u",
+  "overallTodayMinutes": "__R8913_UINT_u",
+  "overallTodayStarts": "__R8952_USINT_u",
+  "overallYesterdayHours": "__R8934_USINT_u",
+  "overallYesterdayMinutes": "__R8935_UINT_u",
+  "overallYesterdayStarts": "__R8953_USINT_u",
+  "hotWaterTotalHours": "__R8944_UDINT_u",
+  "hotWaterTotalStarts": "__R8954_UINT_u",
+  "hotWaterTodayHours": "__R8919_USINT_u",
+  "hotWaterTodayMinutes": "__R8920_UINT_u",
+  "hotWaterTodayStarts": "__R8956_USINT_u",
+  "hotWaterYesterdayHours": "__R8937_USINT_u",
+  "hotWaterYesterdayMinutes": "__R8938_UINT_u",
+  "hotWaterYesterdayStarts": "__R8957_USINT_u",
+
+  # home
+  "zone1SummerMode": "__R15209.0_BOOL_i",
+  "zone2SummerMode": "__R18101.0_BOOL_i",
+  
+  #water
+  "waterState": "__R5439.0_BOOL_i",
+  "waterSwitchingSensorTemperature": "__R64532_REAL_.1f",
+  "waterRequiredTemperature": "__R22883_INT_d",
+  "waterComfortTemperature": "__R5449_REAL_.0f",
+  "waterSetbackTemperature": "__R5453_REAL_.0f",
+  
+  #zone1
+  'zone1State': '__R2357.0_BOOL_i',
+  'zone1Temperature': '__R22661_REAL_.1f',
+  'zone1DesiredTemperature': '__R2359_REAL_.1f',
+
+  #zone2
+  'zone2State': '__R18006.0_BOOL_i',
+  'zone2Temperature': '__R128170_REAL_.1f',
+  'zone2DesiredTemperature': '__R18008_REAL_.1f',
+
+  #aku
+  "akuState": "__R3819.0_BOOL_i",
+  "akuComfortTemperature": "__R3820_USINT_u",
+  "akuSetbackTemperature": "__R3821_USINT_u",
+}
