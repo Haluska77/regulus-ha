511e6fd3af3099a6a057fd567d60b7c719120f82
Stanislav Haluska <stanislav.haluska@gmail.com>
2026-04-26 23:47:45 +0200

    add new mapper

---
+++ custom_components/regulus/mapper/registry_mapper_ir14_1_0_5_0.py
@@ -0,0 +1,67 @@
+REGISTRY_MAPPER = {
+    # dashboard
+    "outdoorTemperature": "__TD9939790_REAL_.1f",
+    "rcTariff": "__T1EAC4A9E_BOOL_i",
+    "holiday": "__T99D3C5D9_BOOL_i",
+
+    "heatPumpOutletTemperature": "__TB483374B_INT_-4.1d",
+    "heatPumpInletTemperature": "__T017CD4D4_INT_-4.1d",
+
+    "heatPumpRunningStatus": "__TF4121779_BOOL_i",
+    "waterRunningStatusFromHeatPump": "__T0E319FDD_BOOL_i",
+    "circulationRunningStatus": "__TDCC7F5B7_BOOL_i",
+
+    # zone 1
+    "zone1RunningStatus": "__TD1499ED4_USINT_u",
+    "zone1Temperature": "__T023DCDC1_REAL_.1f",
+    "zone1DesiredTemperature": "__TAB57C719_REAL_.1f",
+    "zone1HeatingWaterTemperature": "__TEFD93FC9_REAL_.1f",
+    "zone1RequiredHeatingWaterTemperature": "__T023DCDC1_REAL_.1f",
+
+    # zone 2
+    "zone2RunningStatus": "__TC92DBF6B_USINT_u",
+    "zone2Temperature": "__T50E8CD1A_REAL_.1f",
+    "zone2DesiredTemperature": "__T8E3F09A9_REAL_.1f",
+    "zone2HeatingWaterTemperature": "__TA0930ABD_REAL_.1f",
+    "zone2RequiredHeatingWaterTemperature": "__T52FE7A34_REAL_.1f",
+
+    # AKU
+    "akuTopTemperature": "__T0E597DEC_REAL_.1f",
+    "akuBottomTemperature": "__T4C9005EA_REAL_.1f",
+    "akuRequiredTemperature": "__TA629D846_REAL_.1f",
+    "akuRunningStatusFromHeatPump": "__T27B46FA0_BOOL_i",
+    "akuState": "__T27B46FA0_BOOL_i",
+    "akuComfortTemperature": "__TEBD25F9F_REAL_.0f",
+    "akuSetbackTemperature": "__T3C9A6C6F_REAL_.1f",
+
+    # solar
+    "solarPanelTemperature": "__T0769CFB6_REAL_.1f",
+    "solarRunningStatus": "__T42B96F25_BOOL_i",
+
+    # water
+    "waterState": "__T326B0D57_BOOL_i",
+    "waterSwitchingSensorTemperature": "__T29E0FB9B_REAL_.1f",
+    "waterRequiredTemperature": "__T8C9AFCC2_INT_d",
+    "waterComfortTemperature": "__T4AB99A35_REAL_.0f",
+    "waterSetbackTemperature": "__T5A4C89A5_REAL_.0f",
+
+    # heat pump statistics
+    "hpRunningTime": "__T7E09363F_TIME_Thh:mm:ss",
+    "hpIdleTime": "__TCCDFB758_TIME_Thh:mm:ss",
+    "overallTotalHours": "__T2FC939AD_UDINT_u",
+    "overallTotalStarts": "__T0502AE4E_UINT_u",
+    "overallTodayHours": "__T93D9B267_USINT_u",
+    "overallTodayMinutes": "__T3E2DE708_UINT_u",
+    "overallTodayStarts": "__T884463DE_USINT_u",
+    "overallYesterdayHours": "__T0583680F_USINT_u",
+    "overallYesterdayMinutes": "__TA87722EF_UINT_u",
+    "overallYesterdayStarts": "__T5F43F9C4_USINT_u",
+    "hotWaterTotalHours": "__T56A73D11_UDINT_u",
+    "hotWaterTotalStarts": "__T99AE3B59_UINT_u",
+    "hotWaterTodayHours": "__T1F554C11_USINT_u",
+    "hotWaterTodayMinutes": "__TB2A1ECA9_UINT_u",
+    "hotWaterTodayStarts": "__TF12A2D8E_USINT_u",
+    "hotWaterYesterdayHours": "__T992F531F_USINT_u",
+    "hotWaterYesterdayMinutes": "__T34DBF26F_UINT_u",
+    "hotWaterYesterdayStarts": "__T13391633_USINT_u",
+}
